37687f596d1d5dd0b20996e7a05fa368005cc052
Samuel Sutch <sam@sutch.net>
2024-02-05 21:48:13 -0800

    fix api url

--- helpers/api-url.js
+++ helpers/api-url.js
@@ -5,6 +5,6 @@ export function apiUrl() {
         if (process.env.NEXT_PUBLIC_API_URL) {
             return process.env.NEXT_PUBLIC_API_URL;
         }
-        return 'https://chargebeacon.com';
+        return 'https://chargebeacon.app';
     }
 }
